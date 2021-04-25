35839f6c6ca860b928872442e501c0742486fc9e
Doug Jones <DSJR1284@GMAIL.COM>
2021-04-25 17:29:57 -0400

    set up stories table

--- db/migrate/20210425203654_create_stories.rb
+++ db/migrate/20210425203654_create_stories.rb
@@ -1,7 +1,8 @@
 class CreateStories < ActiveRecord::Migration[6.1]
   def change
     create_table :stories do |t|
-
+      t.string :title
+      t.textarea :description 
       t.timestamps
     end
   end
