f06e5e176f0cca53e05beb89f51a7d4580f0c0ac
Doug Jones <DSJR1284@GMAIL.COM>
2021-04-27 16:24:40 -0400

    set up relationship  for story and character

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,5 @@
 class Story < ApplicationRecord
+
+    belongs_to :user 
+    has_many :characters, through: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
 
     has_many :stories
-    has_many :characters, through: :stories  
-
-    
+    has_many :characters, through: :stories    
 end
