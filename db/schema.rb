bc29a4746974e8c54cf3f00a71391592be1bda27
Doug Jones <DSJR1284@GMAIL.COM>
2021-04-29 06:51:36 -0400

    update users attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_224021) do
+ActiveRecord::Schema.define(version: 2021_04_29_104822) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2021_04_28_224021) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
