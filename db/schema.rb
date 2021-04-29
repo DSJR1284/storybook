be3f2e8923d394bdd02704bbc1c0bfac355f3bfd
Doug Jones <DSJR1284@GMAIL.COM>
2021-04-29 07:28:53 -0400

    drop characters controller and update schema migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_104822) do
-
-  create_table "characters", force: :cascade do |t|
-    t.string "name"
-    t.text "bio"
-    t.integer "user_id"
-    t.integer "story_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_04_29_112523) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
