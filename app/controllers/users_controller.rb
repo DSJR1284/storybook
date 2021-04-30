87aff339a855d816fcde1736738ad483847a2f0f
Doug Jones <DSJR1284@GMAIL.COM>
2021-04-30 19:32:52 -0400

    set up signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,28 @@
 class UsersController < ApplicationController
 
     def show
+        @user = User.find_by_id(params[:id])
     end 
 
     def new 
+        if !logged_in?
+            @user = User.new
+        else 
+            redirect_to stories_path
+        end
     end 
 
     def create 
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to stories_path
+        end
+    end 
+
+private 
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
     end 
     
     
