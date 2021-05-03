1887c6f76ee2fa6544b374b8c1c64d68f61ab662
Doug Jones <DSJR1284@GMAIL.COM>
2021-05-03 15:22:48 -0400

    set up login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,12 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(username: params[:username])
-            if user && user.authenticate(params[:password])
-                session[:user_id] = user.id
-                redirect_to user_path(user)
-            else        
-                render :new 
-            end
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to stories_path
+        else        
+            render :new 
+        end
     end 
 
     def logout 
