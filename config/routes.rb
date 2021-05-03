3d4ef15c4bf6e23a6960569605034a80f07b733a
Doug Jones <DSJR1284@GMAIL.COM>
2021-05-03 12:45:38 -0400

    set up storie views

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
 
   post '/logout', to: 'session#destroy'
 
-  resources :comments
-  resources :stories
   resources :users
+  resources :stories
+  resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
