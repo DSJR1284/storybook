53f24637f3f460743843458acd45bee4206b14a0
Doug Jones <DSJR1284@GMAIL.COM>
2021-05-03 16:00:20 -0400

    testing signup log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'application#welcome'
   
   get '/signup', to: 'users#new'
-  post'/signup', to: 'users#create'
-
+  post'/signup', to: 'users#create'  
+  
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
 
