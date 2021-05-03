2bf0418f009131dc9b3216206eefaab335f64be4
Doug Jones <DSJR1284@GMAIL.COM>
2021-05-03 17:23:48 -0400

    add backlink and test login and sign up

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,5 @@
 class StoriesController < ApplicationController
+    before_action :redirect_if_not_logged_in
 
     def show 
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
 
-  post '/logout', to: 'session#destroy'
+  post '/logout', to: 'sessions#destroy'
 
   resources :users
   resources :stories
