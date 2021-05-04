86b94fdbffecfdf67db838eef9448deeea34c60c
Doug Jones <DSJR1284@GMAIL.COM>
2021-05-03 21:38:52 -0400

    set up login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,21 @@ class SessionsController < ApplicationController
         end
     end 
 
+    def omniauth 
+         binding.pry 
+        user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid], provider: request.env['omniauth.auth'][:provider]) do |a|
+            a.username = request.env['omniauth.auth'][:info][:name]
+            a.email = request.env['omniauth.auth'][:info][:email]
+            a.password = SecureRandom.hex(8)
+        end 
+        if user.valid? 
+            session[:user_id] = user.id
+            redirect_to stories_path
+        else 
+            redirect_to root_path
+        end 
+    end 
+
     def destroy
         session.delete :user_id
         redirect_to root_path 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
 
   resources :users
   resources :stories
