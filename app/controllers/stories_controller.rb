b639ce8ffc36186986dd70c13ce975da234e3b3d
Doug Jones <DSJR1284@GMAIL.COM>
2021-05-04 12:13:10 -0400

    working on story model

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,12 +1,12 @@
 class StoriesController < ApplicationController
-    before_action :redirect_if_not_logged_in
+    # before_action :redirect_if_not_logged_in
 
     def show 
 
     end 
 
     def new 
-
+        @story = Story.new 
     end 
 
     def index 
