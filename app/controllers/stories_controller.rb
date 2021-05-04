bd8728a5fe58756e9bfe325976c14638d1973f88
Doug Jones <DSJR1284@GMAIL.COM>
2021-05-04 14:30:21 -0400

    add private method in story controllerm

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -18,8 +18,7 @@ class StoriesController < ApplicationController
         @story.title = params[:title]
         @story.description = params[:description]
         @story.save 
-        
-
+        redirect_to stories_path(@story)
     end 
 
     def edit 
@@ -29,6 +28,17 @@ class StoriesController < ApplicationController
     def update 
 
     end 
+
+    def destroy
+
+    end 
+
+    private 
+    
+    def story_params
+        params.require(:story).permit(:title, :description)
+    end 
+
 
 
 
